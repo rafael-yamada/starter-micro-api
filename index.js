3f6b41d29a9be2dd67e113e4de2887f54d0bcd8c
rafael-yamada <45401309+rafael-yamada@users.noreply.github.com>
2022-12-07 15:21:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,51 +2,23 @@ var http = require('http');
 http.createServer(function (original_request, response) {
     console.log(`Just got a request at ${original_request.url}!`)
 	
-	var https = require('follow-redirects').https;
-	var fs = require('fs');
-
-	var qs = require('querystring');
-
+	var request = require('request');
 	var options = {
 	  'method': 'POST',
-	  'hostname': 'api.infosimples.com',
-	  'path': '/api/v2/consultas/receita-federal/simples-das',
+	  'url': 'https://api.infosimples.com/api/v2/consultas/receita-federal/simples-das',
 	  'headers': {
-		'Content-Type': 'application/x-www-form-urlencoded'
+	    'Content-Type': 'application/x-www-form-urlencoded'
 	  },
-	  'maxRedirects': 20
+	  form: {
+	    'token': 'v9E6v_ecpZQ4VAapkca4bLcHfg5aK1J-z_x6I4zQ',
+	    'cnpj': '43039407000135',
+	    'periodos': '202212'
+	  }
 	};
-
-	var req = https.request(options, function (res) {
-	  var chunks = [];
-
-	  res.on("data", function (chunk) {
-		chunks.push(chunk);
-	  });
-
-	  res.on("end", function (chunk) {
-		var body = Buffer.concat(chunks);
-		console.log(body.toString());
-		
-		response.write(body.toString());
-		
-		response.end();
-	  });
-
-	  res.on("error", function (error) {
-		console.error(error);
-	  });
+	request(options, function (error, response) {
+	  if (error) throw new Error(error);
+	  console.log(response.body);
 	});
-
-	var postData = qs.stringify({
-	  'token': 'v9E6v_ecpZQ4VAapkca4bLcHfg5aK1J-z_x6I4zQ',
-	  'cnpj': '43039407000135',
-	  'periodos': '202212'
-	});
-
-	req.write(postData);
-
-	req.end();
 	
     //response.write('Yo!');
 
