e8d9b11e8a7786729c33ac98736925a55f06b67a
rafael-yamada <45401309+rafael-yamada@users.noreply.github.com>
2022-12-16 09:36:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,31 +2,48 @@ var http = require('http');
 var url = require('url');
 
 http.createServer(function (original_request, final_response) {
-    var q = url.parse(original_request.url, true).query;
-    console.log(`Just got a request at ${original_request.url}!`)
+	var q = url.parse(original_request.url, true).query;
+	console.log(`Just got a request at ${original_request.url}!`)
+	
+	var url = "";
+	var form = {};
+	
+	if (q.servico === "das") {
+		url = 'https://api.infosimples.com/api/v2/consultas/receita-federal/simples-das';
+		form = {
+		    'token': q.token,
+		    'cnpj': q.cnpj,
+		    'periodos': q.periodos
+		}
+	}
+	else if (q.servico === "parcelas") {
+		url = 'https://api.infosimples.com/api/v2/consultas/receita-federal/simples/mei/eg';
+		form = {
+			'token': q.token,
+		    	'cnpj': q.cnpj,
+		   	'cpf': q.cpf,
+			'codigo_acesso': q.chave_acesso,		   
+		}
+	}
 	
 	var request = require('request');
 	var options = {
 	  'method': 'POST',
-	  'url': 'https://api.infosimples.com/api/v2/consultas/receita-federal/simples-das',
+	  'url': url,
 	  'headers': {
 	    'Content-Type': 'application/x-www-form-urlencoded'
 	  },
-	  form: {
-	    'token': q.token,
-	    'cnpj': q.cnpj,
-	    'periodos': q.periodos
-	  }
+	  'form': form
 	};
 	request(options, function (error, response) {
 	  if (error) throw new Error(error);
-		
+
 	  console.log(response.body);
 	  final_response.write(response.body);
 	  final_response.end();
 	});
-	
-    //response.write('Yo!');
 
-    //response.end();
+    	//response.write('Yo!');
+
+    	//response.end();
 }).listen(process.env.PORT || 3000);
